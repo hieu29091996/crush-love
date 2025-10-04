6377aaaef1fdd1d404c4e5882bf12df65574a8f3
hieu29091996 <96782716+hieu29091996@users.noreply.github.com>
2025-10-04 23:37:38 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,11 +1,11 @@
 const CONFIGDATA = {
   titleHeader: "Chào Miuu xinhh, Híu có điều muốn nói. </br> Làm người yêu Híuuu nhé kkk!",
 descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý kkk",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
+    "Hãy trả lời một cách thật lòng nhé. Nếu vợ im lặng và rời đi thì tức là vợ đã ĐỒNG Ý kkk",
+  buttonYes: "Vợ đồng ý",
+  buttonNo: "Cút",
   titleModar: "♥️♥️♥️Cảm ơn Vợ iuuu ♥️♥️♥️",
   descriptionModar:
-    "Cảm ơn em đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ em. </br>Đã lâu rồi anh muốn nói với em điều này: Anh thích em. Không phải thích bình thường, mà là thích rất nhèoo, yêu cực nhiều ♥️</br> Em có biết không, mỗi lần gặp em là tim anh lại đập nhanh hơn một nhịp >.< Sắp tới chân khoẻ lại, cho anh được nắm tay, được ôm em nhé?</br> Love you ♥️♥️♥️",
+    "Cảm ơn em đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br>Đã lâu rồi anh muốn nói với em điều này: Anh thích em. Không phải thích bình thường, mà là thích rất nhèoo, yêu cực nhiều ♥️</br> Em có biết không, mỗi lần gặp em là tim anh lại đập nhanh hơn một nhịp >.< Sắp tới chân khoẻ lại, cho anh được nắm tay, được ôm em nhé?</br> Love you ♥️♥️♥️",
 };
 
